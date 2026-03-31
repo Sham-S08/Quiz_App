28e4f54730641553fc70b93296797a2102cc9718
Shambhavi Srivastava <shambhavi.cd231094@global.org.in>
2026-03-31 15:58:06 +0530

    added deployment updates

--- app.py
+++ app.py
@@ -278,4 +278,5 @@ def on_connect():
 # ─── Run App ─────────────────────────────────────────────────
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, host="0.0.0.0", port=5000)
+
